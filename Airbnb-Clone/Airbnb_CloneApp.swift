eb6f258ba216478176d4b77de9214305327eaf37
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-02-03 00:34:13 +0300

    Initial Commit

---
+++ Airbnb-Clone/Airbnb_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Airbnb_CloneApp.swift
+//  Airbnb-Clone
+//
+//  Created by Dmitry Yastrebov on 03.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Airbnb_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
